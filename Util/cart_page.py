cf6a5bfdd8355f55a8e770c8684bc4e04573b600
Claire Kraft <79609609+ckraft-bot@users.noreply.github.com>
2025-05-08 09:07:00 -0400

    ok modular

---
+++ Util/cart_page.py
@@ -0,0 +1,27 @@
+from Util.base_page import BasePage
+
+class CartPage(BasePage):
+
+    # locators
+    REMOVE_BTN = "//button[@class='button button--link']//span[contains(text(), 'Remove')]"
+
+    def __init__(self, driver):
+        super().__init__(driver)
+
+    def goto(self):
+        self.goto_cart()
+
+    def clear_cart(self):
+        items = self.driver.find_elements_by_xpath(self.REMOVE_BTN)
+        for item in items:
+            
+            max_tries = 3
+            try_num = 0
+            while try_num < max_tries:
+                try:
+                    item.click()
+                    break
+                except Exception as e:
+                    try_num += 1
+            else:
+                raise Exception(f"failed to click on elements:: {locator} after {max_tries} tries")
---
+++ Util/result_page.py
@@ -0,0 +1,32 @@
+from Util.base_page import BasePage
+from selenium.webdriver.support.ui import Select
+
+class ResultPage(BasePage):
+    
+    # locators
+    PRODUCTS_LINK = "//a[@class='product-title-link line-clamp line-clamp-2 truncate-title'][1]"
+    PRODUCTS_PRICE = "//span[@class='price-main-block'][1]"
+    SORT_SELECT = "//div[@class='desktop-bar-sort']//select"
+
+
+    def __init__(self, driver):
+        super().__init__(driver)
+
+    def sort_by_price(self):
+        sort_select = Select(self.driver.find_element_by_xpath(self.SORT_SELECT))
+        sort_select.select_by_visible_text('Price: low to high')
+
+    def choose_product(self, max_price):
+        self.sort_by_price()
+        if not max_price:
+            product = self.driver.find_element_by_xpath(self.PRODUCTS_LINK)
+            product.click()
+        else:
+            price = self.driver.find_element_by_xpath(self.PRODUCTS_PRICE)
+            f_price = price.text.strip()[1:4]
+            if float(f_price) <= float(max_price):
+                product = self.driver.find_element_by_xpath(self.PRODUCTS_LINK)
+                product.click()
+                return True
+            else:
+                return False
